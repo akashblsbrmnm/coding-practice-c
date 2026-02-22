720d275292acd03b0d569830d8071f5f34c4dc8d
Akash <84216042+akashblsbrmnm@users.noreply.github.com>
2026-02-22 08:11:16 +0000

    reorg

--- System/segfault.c
+++
@@ -1,9 +0,0 @@
-// Demonstrate and run a program to create a segfault.
-#include <stdio.h>
-int main()
-{
-    char* str = "Hello, World!";
-    str[0] = 'h';
-    printf("%s\n", str);
-    return 0;
-}
